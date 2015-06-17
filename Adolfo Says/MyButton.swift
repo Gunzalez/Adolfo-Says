4649fac35ba926e5de4219b805b6f46a479f5fd0
gunzalez <gunzalez@hotmail.com>
2015-06-17 23:06:35 +0100

    Adds list view

--- Adolfo Says/MyButton.swift
+++ Adolfo Says/MyButton.swift
@@ -15,10 +15,19 @@ class MyCustomButton: UIButton {
         super.init(coder: aDecoder)
         
         self.layer.cornerRadius = 5.0;
-        self.layer.borderColor = UIColor.redColor().CGColor
+        self.layer.borderColor = UIColor.whiteColor().CGColor
         self.layer.borderWidth = 1.0
-        self.backgroundColor = UIColor.blueColor()
+        self.backgroundColor = UIColorFromRGB(0x8B4513)
         self.tintColor = UIColor.whiteColor()
         
+    }
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 }
